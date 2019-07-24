c19128cc60c02767fbe18f409787b09782078390
William Crutchfield <william.r.crutchfield@gmail.com>
2019-07-23 18:58:52 -0700

    10 Days of Javascript - Day 8: Buttons Container

---
+++ Solutions/Tutorials/10 Days of Javascript/Day 8꞉ Buttons Container/js/buttonsGrid.js
@@ -0,0 +1,11 @@
+var btn5 = document.getElementById('btn5');
+
+let num = [1,2,3,6,9,8,7,4];
+const id = [1,2,3,6,9,8,7,4];
+
+btn5.onclick = () => {
+  num.unshift(num.pop());
+  for (var i = 0; i <= 7; i++) {
+    document.getElementById('btn' + id[i]).innerHTML = num[i];
+  }
+};
